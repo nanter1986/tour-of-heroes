68ac8aa6e11edf194d0711bd6985d757caf5dcbc
nanter1986 <nanter1986@gmail.com>
2017-12-28 09:02:28 +0200

    dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes:Hero[];
-  selectedHero:Hero;
+  
 
   constructor(private heroService: HeroService) { }
 
@@ -23,9 +23,6 @@ export class HeroesComponent implements OnInit {
     .subscribe(heroes=>this.heroes=heroes);
   }
 
-  onSelect(hero:Hero):void{
-    this.selectedHero=hero;
-    this.getHeroes();
-  }
+
 
 }
